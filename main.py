4c2ed8d6911967c962e79342b4f152e66d7b802c
emurphy222000-jpg <78923206+emurphy222000-jpg@users.noreply.github.com>
2022-07-13 13:43:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 
 
 def print_hi(name):
